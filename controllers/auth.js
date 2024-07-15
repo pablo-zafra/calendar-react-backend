6f1d0f236037a6245b90434b1e2c9f6937ed786a
pablozafra90 <pablozafra90@hotmail.com>
2024-07-15 18:32:06 +0200

    383-completado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,37 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async(req, res = response) => {
 
     const { name, email, password } = req.body;
+
+    const usuario = new Usuario( req.body );
+
+    try {
+        
+        await usuario.save();
     
-    //Manejo de errores
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
+        res.status(201).json({
+            ok : true,
+            msg: 'registro',
+        });
+
+    } catch (error) {
+
+        console.log(error);
+        
+        res.status(500).json({
+            ok : false,
+            msg: 'Habla con papá',
+        });
+
     }
 
-    res.status(201).json({
-        ok : true,
-        msg: 'registro',
-        name,
-        email,
-        password,
-    });
 };
 
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body;
-    
-    //Manejo de errores
-    const errors = validationResult( req );
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.json({
         ok : true,
